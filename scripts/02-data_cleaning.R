b7f14b5f79b837a1d59c108b9cdc3387f9559848
Sirui Tan <SiruiTan@utoronto.ca>
2024-01-23 05:33:04 +0000

    data-cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -2,7 +2,7 @@
 # Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
 # Author: Rohan Alexander [...UPDATE THIS...]
 # Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Contact: sirui.tan@utoronto.ca [...UPDATE THIS...]
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
